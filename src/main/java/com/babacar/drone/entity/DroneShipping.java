c7d642704728bedee802fd229a6382515ef2b9c9
Babs95 <babsco95@gmail.com>
2022-10-27 19:26:44 +0000

    adding drone_shipping class

---
+++ src/main/java/com/babacar/drone/entity/DroneShipping.java
@@ -0,0 +1,40 @@
+package com.babacar.drone.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+/**
+ *
+ * @author Babacar
+ * Entity class to map drone_shipping data in h2 database
+ *
+ */
+
+@Entity
+@Table(name = "drone_shipping")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DroneShipping {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name="shipping_address")
+    private String shippingAddress;
+
+    @Column(name="quantity_total")
+    private Integer totalQuantity;
+
+    @Column(name="drone_serial_num")
+    private String droneSerialNum;
+
+    @Column(name="medications_loaded")
+    @OneToMany
+    private List<Medication> medications;
+
+}
